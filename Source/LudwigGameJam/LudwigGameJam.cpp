3fa8b81ffa3a0effadf261094298261e2fb6ac6d
alan93dev <awhitealan@gmail.com>
2023-02-25 12:51:08 -0500

    Initial commit

---
+++ Source/LudwigGameJam/LudwigGameJam.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LudwigGameJam.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LudwigGameJam, "LudwigGameJam" );
